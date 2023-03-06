de79829208d0ed06baa1ecae2b5f25792ec92cc5
IvanVl94 <WlasowIwan@yandex.ru>
2023-03-06 18:18:08 +0300

    Сделано домашка

---
+++ src/homework8/Authors.java
@@ -0,0 +1,17 @@
+package homework8;
+
+public class Authors {
+    private String name;
+    private String surname;
+    public Authors (String name, String surname){
+        this.name = name;
+        this.surname = surname;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public  String getSurname(){
+        return  this.surname;
+    }
+}
+
