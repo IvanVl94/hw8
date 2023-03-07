4514ce31027e46090fe26414594d9a7844555a37
IvanVl94 <WlasowIwan@yandex.ru>
2023-03-07 15:05:05 +0300

    Проверьте, пожалуйста

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,16 @@ import homework8.Book;
             Authors lev = new Authors("Лев", "Толстой");
             Book war = new Book("Война и мир", lev, 1863);
             System.out.println("war.getTitle() = " + war.getTitle());
-            System.out.println("war.getAuthors().getName() = " + lev.getName());
-            System.out.println("war.getAuthors().getSurname() = " + lev.getSurname());
+            System.out.println("war.getAuthors().getName() = " + war.getAuthors().getName());
+            System.out.println("war.getAuthors().getSurname() = " + war.getAuthors().getSurname());
             System.out.println("war.getYear() = " + war.getYear());
             war.setYear(1895);
             System.out.println("war.getYear() = " + war.getYear());
             Authors nik = new Authors("Николай", "Гоголь");
             Book souls = new Book("Мертвые души", nik, 1835);
             System.out.println("souls.getTitle() = " + souls.getTitle());
-            System.out.println("souls.getAuthors().getName() = " + nik.getName());
-            System.out.println("souls.getAuthors().getSurname() = " + nik.getSurname());
+            System.out.println("souls.getAuthors().getName() = " + souls.getAuthors().getName());
+            System.out.println("souls.getAuthors().getSurname() = " + souls.getAuthors().getSurname());
             System.out.println("souls.getYear() = " + souls.getYear());
 
         }
