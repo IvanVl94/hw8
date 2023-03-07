797bd965817f234cb5bd89814553c43df4b2f98e
IvanVl94 <WlasowIwan@yandex.ru>
2023-03-07 14:43:28 +0300

    Сделано домашка2.1

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,20 @@ import homework8.Book;
 
     public class Main {
         public static void main(String[] args) {
-            Book war = new Book("Война и мир", 1863);
+            Authors lev = new Authors("Лев", "Толстой");
+            Book war = new Book("Война и мир", lev, 1863);
             System.out.println("war.getTitle() = " + war.getTitle());
+            System.out.println("lev.getName() = " + lev.getName());
+            System.out.println("lev.getSurname() = " + lev.getSurname());
             System.out.println("war.getYear() = " + war.getYear());
             war.setYear(1895);
             System.out.println("war.getYear() = " + war.getYear());
-            Authors lev = new Authors("Лев", "Толстой");
-            System.out.println("lev.getName() = " + lev.getName());
-            System.out.println("lev.getSurname() = " + lev.getSurname());
-            Book souls = new Book("Мертвые души", 1835);
-            System.out.println("souls.getTitle() = " + souls.getTitle());
-            System.out.println("souls.getYear() = " + souls.getYear());
             Authors nik = new Authors("Николай", "Гоголь");
+            Book souls = new Book("Мертвые души", nik, 1835);
+            System.out.println("souls.getTitle() = " + souls.getTitle());
             System.out.println("nik.getName() = " + nik.getName());
             System.out.println("nik.getSurname() = " + nik.getSurname());
+            System.out.println("souls.getYear() = " + souls.getYear());
+
         }
     }
--- src/homework8/Book.java
+++ src/homework8/Book.java
@@ -2,15 +2,22 @@ package homework8;
 
 public class Book {
     private String title;
+    private Authors authors;
     private int year;
 
-    public Book (String title, int year){
+    public Book (String title, Authors authors, int year){
         this.title = title;
+        this.authors = authors;
         this.year = year;
     }
     public String getTitle(){
         return this.title;
     }
+
+    public Authors getAuthors() {
+        return authors;
+    }
+
     public  int getYear(){
         return this.year;
     }
